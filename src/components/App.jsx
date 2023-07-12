c050aa478e3f156cc53152fd04e52e6d56a29158
OlegatorIT <podlesnyoleg93@gmail.com>
2023-07-12 05:45:31 +0200

    change homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import React from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { Home, Movies, NotFound } from '../pages/index';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Route>
+      </Routes>
+    </>
   );
 };
